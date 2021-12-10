86f2f9de02e59ee503f77cb171ccebed23a3a2c6
popadaniel72 <popadaniel72@yahoo.com>
2021-12-10 10:26:52 +0200

    Add project files.

---
+++ buton_alergaret/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace buton_alergaret
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.X < 10) button1.Left += e.X + 1;
+            if (e.X > button1.Width - 10) button1.Left -= button1.Width - e.X-1;
+        }
+    }
+}
